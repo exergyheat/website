67b7e1a8dfe85355716808a3b44c64d5fc4e07a5
Exergy Admin <admin@exergyheat.com>
2025-06-04 15:51:46 -0600

    Add Our Process button to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -162,6 +162,16 @@ const Home = () => {
               </p>
             </div>
           </div>
+
+          <div className="mt-12 text-center">
+            <Link
+              to="/about"
+              className="inline-flex items-center px-6 py-3 bg-primary-600 text-white rounded-lg hover:bg-primary-700 transition-colors"
+            >
+              Our Process
+              <ArrowRight className="ml-2 h-5 w-5" />
+            </Link>
+          </div>
         </div>
       </div>
 
