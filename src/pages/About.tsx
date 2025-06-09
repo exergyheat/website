5c4d00ced5e49e774dfd4e836c281b2e2a9dab8d
Exergy Admin <admin@exergyheat.com>
2025-06-08 19:18:36 -0600

    Updated About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -226,7 +226,7 @@ const About = () => {
     {
       id: 'monitoring',
       title: 'Remote Monitoring',
-      description: 'Optional remote monitoring ensures your system is always healthy and earning what it should. Because our heaters are digital - we can sense when it's time for maintenance',
+      description: 'Optional remote monitoring ensures your system is always healthy and earning what it should. Because our heaters are digital - we can sense when it is time for a tune up.',
       icon: Monitor,
       preview: {
         title: 'Monitoring Dashboard',
