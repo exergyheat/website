80791910bc1efda33adc273af8320f3999af3e9b
Exergy Admin <admin@exergyheat.com>
2025-06-19 14:29:27 -0600

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -272,7 +272,7 @@ const Home = () => {
                   Why Bitcoin?
                 </h3>
                 <p className="font-body text-surface-600 dark:text-surface-400 text-center">
-                  Bitcoin is digitally native money. It's a neutral protocol - free an open for anyone to use - with no controlling authority.
+                  Bitcoin is digital gold. It's a neutral protocol - free an open for anyone to use - with no controlling authority.
                 </p>
               </div>
             </motion.div>
