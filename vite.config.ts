b0203484f4c94c98388c699898b49cbb61ec66e3
Exergy Admin <admin@exergyheat.com>
2025-06-24 20:02:57 -0600

    Fix Buffer is not defined error

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { Buffer } from 'buffer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'window',
+    Buffer: Buffer,
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
