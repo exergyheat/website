43a5cdbe88b7d484d9a150d0007738c1c015f930
Exergy Admin <admin@exergyheat.com>
2025-06-26 11:17:28 -0600

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -289,7 +289,7 @@ const Home = () => {
                   Does it Heat The Same?
                 </h3>
                 <p className="font-body text-surface-600 dark:text-surface-400 text-center">
-                  Yes. Hashrate heaters can meet all comfort heating needs - while paying you whenever it's used.
+                  Yes. Hashrate heaters can meet all comfort heating needs - while paying you whenever they're used.
                 </p>
               </div>
             </motion.div>
