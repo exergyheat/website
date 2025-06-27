2ec1f5844c6faa9b1a8c9a5872c56e0f49d54092
Exergy Admin <admin@exergyheat.com>
2025-06-27 13:41:50 -0600

    Updated Education.tsx

--- src/pages/Education.tsx
+++ src/pages/Education.tsx
@@ -42,9 +42,9 @@ const Education = () => {
       title: "Decentralized Heat: Bitcoin Mining's Game-Changing Energy Solution W/ Tyler Stevens",
       description: "The Under|Mine HeatPunks Summit is over but the movement continues. This event was one for the history books, the amount of signal that came out of this event was palpable. If you missed the event you’re sure to get a good amount of FOMO from this video which highlights the projects and conversations that are coming out of the heat reuse space. Not only is Hashrate Heating decentralizing Bitcoins Hashrate its also helping to secure the network all while providing much needed heat to homes, businesses and some industrial applications. There is a ton of work to be done if we are going to replace dumb electric heating infrastructure with Bitcoin miners, but luckily we have the HeatPunks dedicated to this mission.",
       type: 'video',
-      author: "Unermine - 2025 Heatpunk Summit Recap Video",
+      author: "Undermine - 2025 Heatpunk Summit Recap Video",
       link: "https://www.youtube.com/watch?v=c-NrYzmPRv8",
-      image: "/image.png",
+      image: "/undermine_recap.webp",
       date: "April 1, 2025"
     }
   ]
