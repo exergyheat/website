50af4ced85c163d273ddf0c9ec61bd84e61cc0b4
Exergy Admin <admin@exergyheat.com>
2025-06-27 15:01:35 -0600

    Updated Portfolio.tsx

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -27,7 +27,7 @@ const projects = [
     id: 'residential',
     title: 'Rural Rocky Mountain Home',
     description: 'Hashrate heating electric boiler for mountain home radiant floor heat.',
-    image: 'https://images.pexels.com/photos/1115804/pexels-photo-1115804.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    image: 'https://travelboulder.com/wp-content/uploads/2021/11/1579-Linden-Dr-Boulder-CO-80304-1.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     category: 'Residential',
     icon: Home,
     stats: {
