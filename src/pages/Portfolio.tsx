0d7bde978f9505493f505764c9116eb5c4158f41
Exergy Admin <admin@exergyheat.com>
2025-06-27 15:04:51 -0600

    Updated Portfolio.tsx

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -46,7 +46,7 @@ const projects = [
   {
     id: 'commercial',
     title: 'Mountain Hot Spring Resort',
-    description: 'Commercial-scale digital boiler solution for a high altitude hot spring resort with supplemental spa heat.',
+    description: 'Commercial-scale digital boiler solution for a high altitude hot spring resort with supplemental spa heat. This custom system replaces a traditional propane boiler, providing greater than 100% cost savings on heating bills.',
     image: 'https://images.squarespace-cdn.com/content/v1/6228169f168e51239c996b76/87583b0a-810c-4a9e-998d-bade1da615e0/0L9A4133.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     category: 'commercial',
     icon: Factory,
