577bebf88ae11ef79bd84950182f1ebba507f3f7
Exergy Admin <admin@exergyheat.com>
2025-06-27 13:39:13 -0600

    Updated Education.tsx

--- src/pages/Education.tsx
+++ src/pages/Education.tsx
@@ -37,6 +37,15 @@ const Education = () => {
       link: "https://www.youtube.com/watch?v=H4faYFvQYOQ",
       image: "/image.png",
       date: "March 20, 2024"
+    },
+    {
+      title: "Decentralized Heat: Bitcoin Mining's Game-Changing Energy Solution W/ Tyler Stevens",
+      description: "In this episode of the Home Mining Podcast, host TwoSats and thermal engineer Tyler Stevens explore the innovative concept of heat reuse in Bitcoin mining. They discuss how Bitcoin miners can serve dual purposes by providing heat for homes and businesses while securing the network. The conversation delves into the technical aspects of heat transfer, real-world applications, and the economic benefits of integrating heat reuse into mining operations. Tyler shares insights on the evolution of ASIC design and the potential for future innovations that prioritize heat reuse, ultimately envisioning a future where Bitcoin mining contributes to self-reliant communities and sustainable energy practices. In this conversation, Tyler Stevens discusses the innovative concept of hash rate heating, which utilizes the heat generated from Bitcoin mining for practical applications such as heating water and spaces. He explains the process of binning in chip manufacturing, the potential of the comfort heating market, and how this could revolutionize energy systems. The discussion also touches on the challenges of chip manufacturing, the importance of aligning incentives for success, and the future landscape of Bitcoin mining as it becomes more decentralized and integrated into home energy systems.",
+      type: 'podcast',
+      author: "Home Mining Podcast by Solo Satoshi",
+      link: "https://www.youtube.com/watch?v=H4faYFvQYOQ",
+      image: "/image.png",
+      date: "March 20, 2024"
     }
   ]
 
