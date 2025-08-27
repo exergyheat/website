d37d79f786cc8aba88e96e1d89fa74dbb4f4dded
Exergy Admin <admin@exergyheat.com>
2025-08-26 20:34:04 -0600

    Updated Services.tsx

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -101,7 +101,7 @@ const Services = () => {
   const getDesignService = () => ({
     id: 'installation',
     name: "Hardware Installation",
-    description: "Hashrte heater installation done right. We work with professional tradesmen tailored to your specific needs and requirements.",
+    description: "Hashrte heater installation done right. We work with professional tradesmen and accommodate your specific needs and requirements.",
     price: "50% Deposit",
     features: [
       "Includes Heat Audit analysis",
