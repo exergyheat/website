27051b232045d86c6ab98b164604775983c6384e
Exergy Admin <admin@exergyheat.com>
2025-08-26 19:50:57 -0600

    Updated mediaHighlights.ts

--- src/data/mediaHighlights.ts
+++ src/data/mediaHighlights.ts
@@ -12,7 +12,7 @@ export const mediaHighlights: MediaHighlight[] = [
     name: 'Forbes',
     logo: '/Forbes_logo.png',
     link: 'https://www.forbes.com/sites/beccabratcher/2025/03/25/bitcoin-miners-hit-breaking-point-one-year-after-halving/',
-    description: 'Mentioned in Forbes for bitcoin mining innovation'
+    description: 'Quoted in Forbes for bitcoin mining innovation'
   },
   {
     id: 'ny-post',
