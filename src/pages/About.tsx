9762f6bd0908da59deb617f5ebc2fea057974364
Exergy Admin <admin@exergyheat.com>
2025-08-26 21:15:35 -0600

    Updated About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -644,7 +644,7 @@ const About = () => {
   const roadmap = [
     {
       quarter: 'Q2 2025',
-      title: 'Soft Launch & Proof of Concepts',
+      title: 'Soft Launch & Proof of Concept Phase',
       events: [
         'Official company launch',
         'Heat Audit & design services development',
