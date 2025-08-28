78f0864bcd0b55c326c54ce96cebebd39430ca90
Exergy Admin <admin@exergyheat.com>
2025-08-28 10:39:51 -0600

    Updated index.html

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+// X (Twitter) widgets type declaration
+declare global {
+  interface Window {
+    twttr: {
+      widgets: {
+        load: (element?: HTMLElement) => void;
+      };
+    };
+  }
+}
+
+export {};
