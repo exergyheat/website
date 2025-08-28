bff499a42ea1fdf57973a63f224c0d73fb820768
Exergy Admin <admin@exergyheat.com>
2025-08-28 09:08:55 -0600

    Updated Products.tsx

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -81,7 +81,7 @@ const Products = () => {
         hashrate: '228 TH/s',
         waterTemp: '158°F max',
         dimensions: '25.1" x 21.5" x 25.2"',
-        capacity: '~1000 sqft Radiant'
+        capacity: 'Small-Med Homes'
       },
       price: '$7299',
       buttonText: 'Contact for Deposit',
