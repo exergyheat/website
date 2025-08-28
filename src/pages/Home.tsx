fc23a9347c476b8333d5a6107225add83559a3dc
Exergy Admin <admin@exergyheat.com>
2025-08-27 21:46:28 -0600

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -286,7 +286,7 @@ const Home = () => {
               </div>
               <h3 className="mt-4 text-xl font-subheading text-surface-900 dark:text-surface-100">Maximum Revenue</h3>
               <p className="mt-2 font-body text-surface-600 dark:text-surface-400">
-                Exergy's Heat Audit analysis optimizes bitcoin heating power to your exact heat demand for maximum earnings and the best recommendation.
+                Exergy's Heat Audit analysis optimizes bitcoin heating power to your exact demand for maximum earnings and the best recommendation.
               </p>
             </div>
           </div>
