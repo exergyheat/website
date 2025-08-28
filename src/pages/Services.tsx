2c7943ad22fc94b2c9ea634f772b3c276cd424cc
Exergy Admin <admin@exergyheat.com>
2025-08-28 13:02:43 -0600

    Updated Services.tsx

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -47,7 +47,7 @@ const Services = () => {
     {
       id: 'consulting',
       title: 'Consult My Team',
-      description: 'Want to master or harness Hashrate Heating for your team?',
+      description: 'Want to master or harness Hashrate Heating for your business?',
       icon: Monitor,
       flipText: 'Start with Remote Health Monitoring if you already have hashrate heat.',
       targetSection: 'consulting-service'
