74884e4d73af4892a0ed40e6132ed4506fc17d17
Exergy Admin <admin@exergyheat.com>
2025-08-31 20:03:55 -0600

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -40,7 +40,7 @@ const Home = () => {
       <Helmet>
         <title>EXERGY - Heat That Pays</title>
         <meta name="description" content="Upgrade to heat that pays you instead. EXERGY's hashrate heating systems generate warmth and bitcoin rewards simultaneously at no extra cost. Vetted hardware, expert design and installation services." />
-        <meta name="keywords" content="hashrate heating, bitcoin mining heat reuse, heat that pays, bitcoin heating, energy efficient heating, bitcoin heaters" />
+        <meta name="keywords" content="hashrate heating, bitcoin mining heat reuse, heat that pays, bitcoin heating, energy efficient heating, bitcoin heaters, electric heat, electric heating, hashrate powered heat, bitcoin mining heat, hash rate heat, hash rate heating, mine for heat, mining for heat" />
       </Helmet>
       
       {/* Hero Section */}
