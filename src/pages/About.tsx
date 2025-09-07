158e0eecc208811ddcdbfaa22c233b7f02e3765e
Exergy Admin <admin@exergyheat.com>
2025-09-07 13:42:16 -0600

    Updated About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -840,10 +840,7 @@ const About = () => {
                 alt="Conference and Summit"
                 className="w-full h-64 sm:h-72 md:h-80 lg:h-96 xl:h-[400px] object-contain object-center rounded-lg shadow-2xl"
               />
-              <div className="absolute -bottom-4 -right-4 bg-white dark:bg-surface-700 p-4 lg:p-6 rounded-lg shadow-xl">
-                <p className="text-xl font-bold text-primary-600 dark:text-primary-400">2026</p>
-                <p className="text-surface-600 dark:text-surface-400">Next Summit</p>
-              </div>
+
             </div>
           </div>
         </div>
