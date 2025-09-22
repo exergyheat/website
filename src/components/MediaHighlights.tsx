0976053dda6fe9c1f1a40b2acf5aec04ab420cd8
Exergy Admin <admin@exergyheat.com>
2025-09-22 08:49:34 -0600

    Updated MediaHighlights.tsx

--- src/components/MediaHighlights.tsx
+++ src/components/MediaHighlights.tsx
@@ -97,7 +97,7 @@ const MediaHighlights = () => {
                 <img
                   src={highlight.logo}
                   alt={`${highlight.name} logo - EXERGY featured in ${highlight.name}`}
-                  className={`${highlight.id === 'imagine-if' ? 'h-12' : 'h-8'} w-auto object-contain transition-all duration-300 ${
+                  className={`${highlight.id === 'imagine-if' ? 'h-8' : 'h-8'} w-auto object-contain transition-all duration-300 ${
                     highlight.id === 'TFTC'
                       ? effectiveTheme === 'dark'
                         ? 'filter invert brightness-50 opacity-60 group-hover:invert group-hover:brightness-100 group-hover:opacity-100'
