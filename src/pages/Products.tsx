ac3db65fcdc49c62228abb9a0fd211ee39baa6bc
Exergy Admin <admin@exergyheat.com>
2025-09-26 11:57:51 -0600

    Updated Products.tsx

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -116,7 +116,7 @@ const Products = () => {
       price: '$1,149',
       couponCode: 'EXERGY',
       buttonText: 'Buy Now at AltairTech.io',
-      buttonLink: '/products',
+      buttonLink: 'https://altairtech.io/product/canaan-avalon-mini-3/',
       description: 'Perfect for heating small to medium-sized rooms while generating passive revenue. The Avalon Mini 3 is easy to setup, can be operated remotely, and has a whisper quiet fan. You won\'t even know it\'s there. Fleet control is coming soon, allowing multiple Mini 3\'s to pair together for larger rooms or full building heating.',
       features: [
         'Simple to follow setup',
